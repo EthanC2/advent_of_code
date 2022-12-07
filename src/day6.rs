0dab67fb7ee83176cf26fc6574ccf03c148b41d3
EthanC2 <ethanrcox@gmail.com>
2022-12-06 22:55:34 -0500

    pushing for mobile tomorrow

--- src/day6.rs
+++ src/day6.rs
@@ -1,13 +1,35 @@
 use std::fs::File;
-use std::io::{BufRead, BufReader, Read};
-use std::collections::HashSet;
+use std::io::{BufReader, Read};
+use std::collections::{HashSet, VecDeque};
 
 #[allow(dead_code)]
 pub fn part1() {
     let file = File::open("input/day6.txt").unwrap();
     let file = BufReader::new(file);
+    let mut chars = HashSet::new();
+    let mut window = VecDeque::new();
     
-    if let Some(Ok(byte)) = file.bytes().last() {
-        println!("{:?}", byte)
+
+    for (index, byte) in file.bytes().enumerate() {
+        let byte = byte.unwrap();
+
+        if chars.contains(&byte) {
+            window.clear();
+            chars.clear();
+        } 
+
+        if let Some(front) = window.pop_front() {
+            chars.remove(front);
+        }
+
+        window.push_back(&byte);
+        chars.insert(byte);
+        
+
+        println!(" - len={}", chars.len());
+        if chars.len() == 4 {
+            println!("Start of message found at index {}", index-2);
+            break;
+        }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,6 @@
 #![feature(iter_array_chunks)]
 #![feature(iter_advance_by)]
 
-mod day1;
-mod day2;
-mod day3;
-mod day4;
-mod day5;
 mod day6;
 
 fn main() {
